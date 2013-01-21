23ddd593887a25d6cef25ec64a72d827b3993665
Akash Khandelwal <er.akashkhandelwal@gmail.com>
2013-01-21 09:09:05 +0530

    Modify sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,18 @@ class SessionsController < ApplicationController
 
 	def create
 		writer = Writer.find_by_email(params[:session][:email].downcase)
-    if writer && writer.authenticate(params[:session][:password]) 
+		if writer && !writer.password_digest.nil?
+			if writer.authenticate(params[:session][:password]) 
 	      sign_in writer
 	      redirect_to writer
-    else
-      flash.now[:error] = 'Invalid email/password combination'
-      render 'new'
-    end
+	    else
+	      flash.now[:error] = 'Invalid email/password combination'
+	      render 'new'
+	    end
+	  else
+	  	flash.now[:error] = 'Please register yourself through email invitation sent to you'
+	  	render 'home/show'
+		end
 	end
 
 	def destroy
